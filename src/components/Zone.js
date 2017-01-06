d774bb4bb8908fba1bc3b8ff6056d3b613d54c1d
Ken Torry <kentorry@afrodesiamedia.com>
2017-01-06 00:45:07 -0600

    added leftover stuff

--- src/components/Zone.js
+++ src/components/Zone.js
@@ -4,10 +4,10 @@ class Zone extends React.Component{
 	render(){
 		return(
 			<div className="Zone panel panel-info">		
-				<div className="panel-heading"><a href="#">Zone 1</a></div>
+				<div className="panel-heading"><a href="#">{this.props.zone.name}</a></div>
 				<div className="panel-body">
-				<p>60660</p>
-				<p>15 comments</p>
+				<p>{this.props.zone.zipCode}</p>
+				<p>{this.props.zone.commentCount} comments</p>
 				</div>
 			</div>
 		)
--- src/components/Zones.js
+++ src/components/Zones.js
@@ -4,16 +4,22 @@ import Zone from './Zone'
 
 class Zones extends React.Component{
 	render(){
+
+		const firstZone = {name:'Zone 1', zipCode: '60153', numComments:14}
+		const secondZone = {name:'Zone 2', zipCode: '60551', numComments:33}
+		const thirdZone = {name:'Zone 3', zipCode: '60621', numComments:150}
+		const fourthZone = {name:'Zone 4', zipCode: '60458', numComments:51}
+
 		return(
 			<div className="Zones container">
 				<h2>Zones</h2>
 				
 				<ol>
 
-					<li> <Zone/> </li>
-					<li> <Zone/> </li>
-					<li> <Zone/> </li>
-					<li> <Zone/> </li>
+					<li> <Zone zone={firstZone} /> </li>
+					<li> <Zone zone={secondZone} /> </li>
+					<li> <Zone zone={thirdZone} /> </li>
+					<li> <Zone zone={fourthZone} /> </li>
 					
 				</ol>
 			</div>
